4cd4b1c1f3f699988d28e6da344a029094d5d1c0
ceowis <ij17279@naver.com>
2024-01-03 23:43:15 +0900

    realgrid 버전변경 및 버그수정

--- mobx_sample/src/index.tsx
+++ mobx_sample/src/index.tsx
@@ -25,9 +25,9 @@ import React from 'react';
 import Root from './routes/root.tsx';
 import ErrorPage from './error-page.tsx';
 import Contact from './routes/contact.tsx';
-import MobxSample from './routes/MobxSample.tsx';
-import GridSample from './routes/GridSample.tsx';
 import App from './App.tsx';
+import GridSample from './routes/GridSample.tsx';
+import MobxSample from './routes/MobxSample.tsx';
 
 const realGrid2Lic =
   'upVcPE+wPOmtLjqyBIh9RkM/nBOseBrflwxYpzGZyYm9cY8amGDkiMnVeQKUHJDjW2y71jtk+wte7L7C4dZzvFRtu9Bz90f9w1ScDYRVGVc=';
